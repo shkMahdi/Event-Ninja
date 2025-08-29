405f069591e06f0142f9aa9f5ace103218e01ac1
Shekh Mahdi Mesbah <mahdisheikh1624@gmail.com>
2025-08-29 15:17:17 +0600

    add event time and location fields to meta box

--- event-ninja.php
+++ event-ninja.php
@@ -79,18 +79,30 @@ class EventNinja {
      */
     public function event_details_callback($post) {
         wp_nonce_field('en_save_event_meta', 'en_event_nonce');
-        
+    
         $event_date = get_post_meta($post->ID, '_en_event_date', true);
+        $event_time = get_post_meta($post->ID, '_en_event_time', true);
+        $event_location = get_post_meta($post->ID, '_en_event_location', true);
         ?>
-        <p>
-            <label for="en_event_date">Event Date:</label>
-            <input type="date" id="en_event_date" name="en_event_date" value="<?php echo esc_attr($event_date); ?>" />
-        </p>
+        <table class="form-table">
+            <tr>
+                <th><label for="en_event_date">Event Date</label></th>
+                <td><input type="date" id="en_event_date" name="en_event_date" value="<?php echo esc_attr($event_date); ?>" /></td>
+            </tr>
+            <tr>
+                <th><label for="en_event_time">Event Time</label></th>
+                <td><input type="time" id="en_event_time" name="en_event_time" value="<?php echo esc_attr($event_time); ?>" /></td>
+            </tr>
+            <tr>
+                <th><label for="en_event_location">Location</label></th>
+                <td><input type="text" id="en_event_location" name="en_event_location" value="<?php echo esc_attr($event_location); ?>" /></td>
+            </tr>
+        </table>
         <?php
     }
 
+
     public function save_event_meta($post_id) {
-        // Security checks
         if (!isset($_POST['en_event_nonce']) || !wp_verify_nonce($_POST['en_event_nonce'], 'en_save_event_meta')) {
             return;
         }
@@ -101,6 +113,12 @@ class EventNinja {
         
         if (isset($_POST['en_event_date'])) {
             update_post_meta($post_id, '_en_event_date', sanitize_text_field($_POST['en_event_date']));
+        }
+        if (isset($_POST['en_event_time'])) {
+            update_post_meta($post_id, '_en_event_time', sanitize_text_field($_POST['en_event_time']));
+        }
+        if (isset($_POST['en_event_location'])) {
+            update_post_meta($post_id, '_en_event_location', sanitize_text_field($_POST['en_event_location']));
         }
     }
 
